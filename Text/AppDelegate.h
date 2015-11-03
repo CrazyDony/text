efcae65eaf2e4fb259ea47283ce595d9fc8a086b
tcrj <tcrj@bogon>
2015-11-03 10:33:03 +0800

    Initial Commit

---
+++ Text/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Text
+//
+//  Created by tcrj on 15/11/3.
+//  Copyright © 2015年 CrazyDony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Text/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text
+//
+//  Created by tcrj on 15/11/3.
+//  Copyright © 2015年 CrazyDony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
